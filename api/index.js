12fdcf6b2811687dd3e159fbc717f81965c52b7a
sathish <me.sathish.git@gmail.com>
2025-05-28 23:12:17 +0530

    setup static file hosting

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
