0ba26a8453354c47df22018e18363b431b603f11
sathish <me.sathish.git@gmail.com>
2025-05-29 00:01:24 +0530

    setup cors

--- api/index.js
+++ api/index.js
@@ -1,15 +1,36 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
+
 const products = require("./public/ice-creams/products.json");
 const reviews = require("./public/ice-creams/reviews.json");
 
 const app = express();
 
+// Allow only a specific domain
+const allowedDomains = [
+  "https://frostbite-flax.vercel.app",
+  "http://localhost:4200",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedDomains.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) =>
   res.send(
-    "Backend for frostbite ice cream shop... </br> /api/products for all products. <br> /api/reviews/:key for reviews bassed on the product's key. </br> /ice-creams/images/{ product_key }.png for images."
+    "Backend for frostbite ice cream shop... </br> /api/products for all products. <br> /api/reviews/:key for reviews based on the product's key. </br> /ice-creams/images/{ product_key }.png for images."
   )
 );
 
